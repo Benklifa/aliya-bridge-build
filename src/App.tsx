e55ae99417052f70722b19ad157546a82ec63968
Aliya Financial <admin@aliyafinancial.com>
2025-10-21 09:37:04 -0400

    Add ScrollToTop component to fix navigation scroll position

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import Contact from "./pages/Contact";
 import FAQ from "./pages/FAQ";
 import ReadinessScore from "./pages/ReadinessScore";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -27,6 +28,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/framework" element={<Framework />} />
